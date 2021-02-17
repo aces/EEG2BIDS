59414b6819b8abc80e9fab859ec4112f05e36d4e
maltheism <misotheism@users.noreply.github.com>
2021-02-17 10:25:02 -0500

    update

--- src/jsx/Welcome.js
+++ src/jsx/Welcome.js
@@ -21,22 +21,34 @@ const Welcome = (props) => {
       </div>
       <div style={{backgroundColor: '#039b83', padding: '10px'}}>
         <p style={{fontSize: '16pt'}}>Hello user,</p>
-        <p><b>pyCat</b> is a simple tool for de-identification of iEEG datasets.
+        <p style={{fontSize: '14pt'}}><b>pyCat</b>&nbsp;
+          is a simple tool for de-identification of iEEG datasets.
           In addition to de-identifying iEEG files, this tool contains a feature
-          that allows mapping the candidate's information to its
+          that allows mapping the LORIS candidate's information to its
           study identifier.
         </p>
         <p>
           <b>The de-identifier tab allows to:</b>
         </p>
+        <ul>
+          <li>todo ...</li>
+          <li>todo ...</li>
+          <li>todo ...</li>
+        </ul>
         <p>
           <b>The iEEG Converter tab allows to:</b>
         </p>
+        <ul>
+          <li>todo ...</li>
+          <li>todo ...</li>
+        </ul>
         <p>
           <b>The Validator tab allows to:</b>
         </p>
-        <br/>
-        <p>
+        <ul>
+          <li>todo ...</li>
+        </ul>
+        <p style={{fontSize: '14pt'}}>
           You may begin your task by following the menu above.
           Please remember to always backup your data!
         </p>
