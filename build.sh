c5b5ef7ab6675d15f389afb0120fa2fa798aa479
maltheism <misotheism@users.noreply.github.com>
2021-03-27 03:09:07 -0400

    fix build

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 source bin/activate
 pyinstaller --paths=python python/pycat.py -F \
 --name pycat-service \
+--add-data 'python/libs/bids_validator/rules/top_level_rules.json:bids_validator/rules' \
+--add-data 'python/libs/bids_validator/rules/associated_data_rules.json:bids_validator/rules' \
+--add-data 'python/libs/bids_validator/rules/file_level_rules.json:bids_validator/rules' \
+--add-data 'python/libs/bids_validator/rules/phenotypic_rules.json:bids_validator/rules' \
+--add-data 'python/libs/bids_validator/rules/session_level_rules.json:bids_validator/rules' \
+--add-data 'python/libs/bids_validator/rules/subject_level_rules.json:bids_validator/rules' \
+--add-data 'python/libs/bids_validator/tsv/non_custom_columns.json:bids_validator/tsv' \
 --hidden-import=eventlet.hubs.epolls \
 --hidden-import=eventlet.hubs.kqueue \
 --hidden-import=eventlet.hubs.selects \
