41dc836c736a17b13c7bb24bf69e49b0cab5457e
maltheism <misotheism@users.noreply.github.com>
2021-04-06 23:47:48 -0400

    update

--- public/electron.js
+++ public/electron.js
@@ -13,7 +13,7 @@ const PycatService = process.env.DEV ?
 
 // Launch python service.
 const pycatService = new PycatService();
-pycatService.startup();
+// pycatService.startup();
 
 if (process.env.DEV) {
   const {
