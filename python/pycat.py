25bd834bdce2303af1bef6deef5a8cfa82aa75fd
maltheism <misotheism@users.noreply.github.com>
2021-04-07 13:03:07 -0400

    update

--- python/pycat.py
+++ python/pycat.py
@@ -18,12 +18,14 @@ def connect(sid, environ):
 
 @sio.event
 def tarfile_bids(sid, data):
+    # data = { bids_directory: '../path/to/bids/output', output_time: 'bids output time' }
     print('tarfile_bids:', data)
     iEEG.TarFile(data)
 
 
 @sio.event
 def ieeg_get_header(sid, data):
+    # data = { file_path: 'path to iEEG file' }
     print('ieeg_get_header:', data)
     try:
         anonymize = iEEG.Anonymize(data)
@@ -41,6 +43,9 @@ def ieeg_get_header(sid, data):
 
 @sio.event
 def edf_to_bids(sid, data):
+    # data = { file_path: '', bids_directory: '', read_only: false,
+    # events_tsv: '', line_freq: '', site_id: '', project_id: '',
+    # sub_project_id: '', visit_label: '', subject_id: ''}
     print('edf_to_bids: ', data)
     error_messages = []
     if not data['file_path']:
@@ -82,6 +87,7 @@ def edf_to_bids(sid, data):
 
 @sio.event
 def validate_bids(sid, data):
+    # data = { bids_directory: '../path/to/bids/output', output_time: 'bids output time' }
     print('validate_bids: ', data)
     error_messages = []
     if not data['bids_directory']:
