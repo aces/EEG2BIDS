77f19c14b1dfab5f13ceebe8ee9688fab7ce0c3b
Tyler Collins <tk11br@sharcnet.ca>
2026-07-18 17:11:02 -0400

    purge old edf code

--- eeg2bids/iEEG.py
+++ eeg2bids/iEEG.py
@@ -1,6 +1,5 @@
 import os
 import mne
-from eeg2bids import EDF
 from mne_bids import write_raw_bids, BIDSPath
 
 
@@ -213,44 +212,6 @@ class TarFile:
         #    subprocess.Popen(['open', data['bids_directory']])
         #else:
         #    subprocess.Popen(['xdg-open', data['bids_directory']])
-
-
-# Anonymize - scrubs edf header data.
-class Anonymize:
-    file_path = ''
-    header = []
-
-    # data = { file_path: 'path to iEEG file' }
-    def __init__(self, file_path):
-        self.file_path = file_path
-
-        try:
-            # read EDF file from file_path,
-            file_in = EDF.EDFReader(fname=self.file_path)
-            # read header of EDF file.
-            self.header = file_in.readHeader()
-            file_in.close()
-        except PermissionError as ex:
-            raise ReadError(ex)
-
-    def get_header(self):
-        return self.header
-
-    def set_header(self, key, value):
-        self.header[0][key] = value
-
-    def make_copy(self, new_file):
-        header = self.get_header()
-        file_in = EDF.EDFReader(fname=self.file_path)
-        file_out = EDF.EDFWriter()
-        file_out.open(new_file)
-        file_out.writeHeader(header)
-        meas_info = header[0]
-        for i in range(meas_info['n_records']):
-            data = file_in.readBlock(i)
-            file_out.writeBlock(data)
-        file_in.close()
-        file_out.close()
 
 
 # Converter - Creates the BIDS output by edf file.
